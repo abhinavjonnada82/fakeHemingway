5e911ed6e1ddf23bb97670cac78af2ea2b45edbc
abhinavjonnada82 <abhinav.jonnada@gmail.com>
2019-08-18 05:04:05 -0400

    basic data management taken care, need to implement actuall functionality

--- src/App.js
+++ src/App.js
@@ -5,35 +5,70 @@ import OutputAreaField from "./components/outputField";
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
+const style = {
+    position: 'absolute',
+    top: '230px'
+};
+
 class App extends Component  {
-  render() {
-    return (
-        <div>
-          <div className="App-header">
-              <h1>
-                Fake Hemingway
-              </h1>
-          </div>
-            <div className="container">
-                <div className="row">
-                    <div className="col-xl-8 ">
-                        <TextAreaField />
-                    </div>
-                    <div className="col-xl-4">
-                        <CounterAreaField />
-                    </div>
-                    <br />
-                    <div className="container">
+    constructor(props){
+        super(props)
+        this.state = {
+            description: " "
+        }
+        this.textChangeHandler = this.textChangeHandler.bind(this);
+        this.submitHandler = this.submitHandler.bind(this);
+    }
+    textChangeHandler = (event) => {
+        this.setState({ description: event.target.value })
+    }
+    submitHandler = (event) => {
+        event.preventDefault();
+    }
+      render() {
+        return (
+            <div>
+              <div className="App-header">
+                  <h1>
+                    Fake Hemingway
+                  </h1>
+              </div>
+                <div className="container">
                     <div className="row">
-                        <div className="col-lg-8 ">
-                            <OutputAreaField />
+                        <div className="col-xl-8 ">
+                            <div className="align-field">
+                                <form onSubmit={this.submitHandler}>
+                                    <div className="form-group">
+                                        <textarea
+                                        className ="text-area form-control"
+                                        type="text"
+                                        onChange = {this.textChangeHandler}
+                                    />
+                                    </div>
+                                    <br />
+                                    <div className="form-group" style = {style}>
+                                        <input  type="submit" className="btn btn-primary btn" value="Submit"  />
+                                    </div>
+                                </form>
+                                <br />
+                            </div>
+                        </div>
+
+                        <div className="col-xl-4">
+                            <CounterAreaField/>
+                        </div>
+                        <br />
+                        <div className="container">
+                        <div className="row">
+                            <div className="col-lg-8 ">
+                                <OutputAreaField  description={this.state.description} />
+                            </div>
+                        </div>
                         </div>
                     </div>
-                    </div>
                 </div>
-            </div>
 
-        </div>
+            </div>
 
 
     );
--- src/components/outputField.js
+++ src/components/outputField.js
@@ -1,13 +1,52 @@
 import React, { Component } from "react";
 
+// const outputAreaField = props => (
+//     <div className="card output-area">
+//         <div className="card-body">
+//             <p>{props.description}</p>
+//         </div>
+//     </div>
+// )
+let data = {
+    paragraphs: 0,
+    sentences: 0,
+    words: 0,
+    hardSentences: 0,
+    veryHardSentences: 0,
+    adverbs: 0,
+    passiveVoice: 0,
+    complex: 0
+};
+
 class outputAreaField extends Component  {
+    getDifficultSentences(p) {
+        console.log(p)
+    }
     render() {
+        data = {
+            paragraphs: 0,
+            sentences: 0,
+            words: 0,
+            hardSentences: 0,
+            veryHardSentences: 0,
+            adverbs: 0,
+            passiveVoice: 0,
+            complex: 0
+        };
+        let text = this.props.description
+        let paragraphs = text.split("\n")
+        let hardSentences = paragraphs.map(p => this.getDifficultSentences(p));
+        // let inP = hardSentences.map(para => `<p>${para}</p>`);
+        data.paragraphs = paragraphs.length;
+        console.log(data.paragraphs);
         return (
-            <div className="card counter-area">
+            <div className="card output-area">
                 <div className="card-body">
-                    <h1>Hello World!</h1>
+                    <p>{ paragraphs }</p>
+                    {/*<p>{ inP.join(" ") }</p>*/}
                 </div>
             </div>
+
         );
     }
 }
